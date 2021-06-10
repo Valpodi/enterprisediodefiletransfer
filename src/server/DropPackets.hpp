21535c5ac60f1855cd940114679431c93bc95494
Jamie McDonald <Jamie.McDonald@paconsulting.com>
2021-06-10 16:34:08 +0000

    Print timestamps of dropped packets and number of packets dropped

--- src/server/DropPackets.hpp
+++ src/server/DropPackets.hpp
@@ -1,6 +1,7 @@
 // Copyright PA Knowledge Ltd 2021
 // MIT License. For licence terms see LICENCE.md file.
 
+#include <chrono>
 #include <iostream>
 
 class StreamInterface;
@@ -8,8 +9,16 @@ class StreamInterface;
 class DropPackets : public PacketQueueInterface
 {
 public:
-  bool write(std::istream&, StreamInterface*, std::uint32_t, bool eOFFlag) override
+  bool write(std::istream&, StreamInterface*, std::uint32_t frameCount, bool eOFFlag) override
   {
+    if (frameCount != nextFrameCount)
+    {
+      std::cout << std::chrono::system_clock::now().time_since_epoch().count() << " Out of order packet. Got frame no: " << frameCount << "; Number of frames out of order: " << frameCount - nextFrameCount << "\n";
+      nextFrameCount = frameCount;
+    }
+    ++nextFrameCount;
     return eOFFlag;
   };
+
+  std::uint32_t nextFrameCount = 1;
 };
--- src/server/ReorderPacketsTests.cpp
+++ src/server/ReorderPacketsTests.cpp
@@ -288,15 +288,19 @@ TEST_CASE("ReorderPackets. Import diode.")
   }
 }
 
-TEST_CASE("DropPackets. Packets received in any order are not written to the output")
+TEST_CASE("DropPackets.")
 {
-  auto inputStream = std::stringstream("BC");
   std::stringstream outputStream;
   StreamSpy stream(outputStream, 1);
   auto queueManager = DropPackets();
-  REQUIRE_FALSE(queueManager.write(inputStream, &stream, 1, false));
 
-  inputStream = std::stringstream("DE");
-  REQUIRE(queueManager.write(inputStream, &stream, 2, true));
-  REQUIRE(outputStream.str().empty());
+  SECTION("Packets received in any order are not written to the output")
+  {
+    auto inputStream = std::stringstream("BC");
+    REQUIRE_FALSE(queueManager.write(inputStream, &stream, 1, false));
+
+    inputStream = std::stringstream("DE");
+    REQUIRE(queueManager.write(inputStream, &stream, 2, true));
+    REQUIRE(outputStream.str().empty());
+  }
 }
