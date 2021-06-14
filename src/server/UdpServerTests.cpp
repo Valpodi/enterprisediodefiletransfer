48af7913d2b1fe8b913f463c03cdc4824907bd7d
James Paley <james.paley@paconsulting.com>
2021-06-14 13:11:57 +0000

    test to move a vector buffer  - updated tests

--- src/server/UdpServerTests.cpp
+++ src/server/UdpServerTests.cpp
@@ -16,16 +16,14 @@ TEST_CASE("UDP Server. Packets are received", "[integration]")
 {
   std::vector<char> dataReceived;
 
-  std::stringstream stuff;
-
   boost::asio::io_service io_context;
-  UdpServer udpServer(2002, io_context, 10);
+  UdpServer udpServer(2002, io_context, 150);
   udpServer.setCallback([&io_context, &dataReceived](BytesBuffer&&, BytesBuffer&& data) {
     std::copy(data.begin(), data.end(), std::back_inserter(dataReceived));
     io_context.stop();
   });
 
-  std::vector<char> sendPayload(112 + 3);
+  std::vector<char> sendPayload(112);
   sendPayload.push_back('A');
   sendPayload.push_back('B');
   sendPayload.push_back('C');
