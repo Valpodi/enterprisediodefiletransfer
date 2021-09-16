bb7dbdd11182425df3905c6b4e98d3833024bed5
Hardeep Sharma <hardeep.sharma@paconsulting.com>
2021-09-16 14:09:48 +0100

    WIP update ServerTests.cpp for multi-thread

--- src/server/ServerTests.cpp
+++ src/server/ServerTests.cpp
@@ -11,6 +11,8 @@
 #include "Server.hpp"
 #include "StreamSpy.hpp"
 
+#define WAIT_FOR_ASYNC_THREAD usleep(10000)
+
 TEST_CASE("ED server.")
 {
   std::stringstream outputStream;
@@ -24,44 +26,45 @@ TEST_CASE("ED server.")
   SECTION("A packet received by the server is written to the stream")
   {
     edServer.receivePacket(createTestPacketStream(1, 1, false), {'B'});
-
+    WAIT_FOR_ASYNC_THREAD;
     REQUIRE(outputStream.str() == std::string("B"));
 
     const std::string filename = "{name: !str \"testFilename\"}";
     edServer.receivePacket(createTestPacketStream(1, 2, true), {filename.begin(), filename.end()});
-
+    WAIT_FOR_ASYNC_THREAD;
     REQUIRE(outputStream.str() == std::string("B"));
   }
 
   SECTION("Packets received are inserted into the same stream until the EOF flag is raised")
   {
     edServer.receivePacket(createTestPacketStream(1, 1, false), {'B', 'C'});
-
+    WAIT_FOR_ASYNC_THREAD;
     REQUIRE(outputStream.str() == std::string("BC"));
 
     edServer.receivePacket(createTestPacketStream(1, 2, false), {'D'});
-
+    WAIT_FOR_ASYNC_THREAD;
     REQUIRE(outputStream.str() == std::string("BCD"));
     std::string filename = "{name: !str \"testFilename\"}";
     edServer.receivePacket(createTestPacketStream(1, 3, true), {filename.begin(), filename.end()} );
-
+    WAIT_FOR_ASYNC_THREAD;
     REQUIRE(outputStream.str() == std::string("BCD"));
   }
 
   SECTION("Packets are reordered before being written to stream")
   {
     edServer.receivePacket(createTestPacketStream(1, 2, false), {'C', 'D'});
-
+    WAIT_FOR_ASYNC_THREAD;
     REQUIRE(outputStream.str() == std::string(""));
 
     const std::string filename = "{name: !str \"testFilename\"}";
     edServer.receivePacket(createTestPacketStream(1, 3, true), {filename.begin(), filename.end()});
-
+    WAIT_FOR_ASYNC_THREAD;
     REQUIRE(outputStream.str() == std::string(""));
 
     SECTION("The first frame is received and both are written to the output")
     {
       edServer.receivePacket(createTestPacketStream(1, 1, false), {'A', 'B'});
+      WAIT_FOR_ASYNC_THREAD;
       REQUIRE(outputStream.str() == std::string("ABCD"));
     }
   }
@@ -69,13 +72,13 @@ TEST_CASE("ED server.")
   SECTION("Packets with invalid headers are not written to the output")
   {
     edServer.receivePacket(BytesBuffer(EnterpriseDiode::HeaderSizeInBytes - 1), {});
-
+    WAIT_FOR_ASYNC_THREAD;
     REQUIRE(outputStream.str() == std::string(""));
 
     SECTION("Subsequent packet with valid header is written to the output")
     {
       edServer.receivePacket(createTestPacketStream(1, 1, false),{'X', ' '} );
-
+      WAIT_FOR_ASYNC_THREAD;
       REQUIRE(outputStream.str() == std::string("X "));
     }
   }
@@ -83,7 +86,7 @@ TEST_CASE("ED server.")
   SECTION("Session ID is passed to the stream manager")
   {
     edServer.receivePacket(createTestPacketStream(2, 1, false), {'B', 'C'});
-
+    WAIT_FOR_ASYNC_THREAD;
     REQUIRE(capturedSessionId == 2);
     REQUIRE(outputStream.str() == std::string("BC"));
   }
@@ -91,7 +94,7 @@ TEST_CASE("ED server.")
   SECTION("Basic Diode server does not write the CDHeader even if it implies wrapping")
   {
     edServer.receivePacket(createTestPacketStream(1, 1, false, true), {'A'});
-
+    WAIT_FOR_ASYNC_THREAD;
     REQUIRE(outputStream.str() == "A");
   }
 }
@@ -108,29 +111,34 @@ TEST_CASE("ED server. Import Diode")
   SECTION("Blank CDHeader and starting bmp char is written to disk without CDHeader")
   {
     edServer.receivePacket(createTestPacketStream(1, 1, false, false), {'B'});
+    WAIT_FOR_ASYNC_THREAD;
     REQUIRE(outputStream.str() == "B");
   }
 
   SECTION("Blank CDHeader and starting sisl char is written to disk without CDHeader")
   {
     edServer.receivePacket(createTestPacketStream(1, 1, false, false), {'{'});
+    WAIT_FOR_ASYNC_THREAD;
     REQUIRE(outputStream.str() == "{");
   }
 
   SECTION("Blank CDHeader and starting char not sisl or bmp char is not written.")
   {
     edServer.receivePacket(createTestPacketStream(1, 1, false, false), {'A'});
+    WAIT_FOR_ASYNC_THREAD;
     REQUIRE(outputStream.str().empty());
   }
 
   SECTION("Import Diode server writes the CDHeader if it is the first frame and is wrapped")
   {
     edServer.receivePacket(createTestPacketStream(1, 1, false, true), {'\x11'});
+    WAIT_FOR_ASYNC_THREAD;
     REQUIRE(outputStream.str() == CDWrappedHeaderString + "\x11");
 
     SECTION("Check the second wrapped packet is rewrapped and the CDHeader is not rewritten")
     {
       edServer.receivePacket(createTestPacketStream(1, 2, false, true), {'\x12'});
+      WAIT_FOR_ASYNC_THREAD;
       REQUIRE_FALSE(outputStream.str() == CDWrappedHeaderString + "\x11\x12");
       REQUIRE(outputStream.str().size() == CDWrappedHeaderString.size() + 2);
     }
@@ -155,6 +163,7 @@ TEST_CASE("ED server. Queue length Tests")
     std::string filename = "{name: !str \"testFilename\"}";
     edServer.receivePacket(createTestPacketStream(1, 6, true), {filename.begin(), filename.end()});
     edServer.receivePacket(createTestPacketStream(1, 2, false), {'C', 'D'});
+    WAIT_FOR_ASYNC_THREAD;
     REQUIRE(outputStream.str() == std::string("AB"));
   }
 }
@@ -172,10 +181,12 @@ TEST_CASE("ED server. Stream is closed if timeout is exceeded.", "[integration]"
                            }, []() { return std::time(nullptr); }, 3, DiodeType::basic);
 
   edServer.receivePacket(createTestPacketStream(1, 1, false), {'A', 'B'});
+  WAIT_FOR_ASYNC_THREAD;
   REQUIRE(outputStream.str() == std::string("AB"));
   sleep(4);
 
   std::string filename = "{name: !str \"testFilename\"}";
   edServer.receivePacket(createTestPacketStream(1, 2, true), {filename.begin(), filename.end()});
+  WAIT_FOR_ASYNC_THREAD;
   REQUIRE(outputStream.str() == std::string("AB"));
 }
